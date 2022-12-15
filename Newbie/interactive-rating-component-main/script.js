69b115ac874c7ce0444c922609ba9580965effd3
Mariana Maia <marianalima.amb@gmail.com>
2022-12-15 11:02:50 -0300

    completed

--- Newbie/interactive-rating-component-main/script.js
+++ Newbie/interactive-rating-component-main/script.js
@@ -4,19 +4,6 @@ const form = document.querySelector(".rating-form");
 const selector = "input[name=ratings]:checked";
 const checkedInput = document.querySelector(selector);
 
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   if (checkedInput !== null) {
-//     const selectedRating = document.querySelector("#selection");
-//     selectedRating.textContent =
-//       "You selected " + checkedInput.getAttribute("value") + " out of 5";
-
-//   }
-// }
-
-// const submitBtn = document.querySelector("#button__submit");
-// submitBtn.addEventListener("click", handleSubmit);
-
 form.addEventListener("submit", function (e) {
   e.preventDefault(); //prevents Browser from refreshing
   const selector = "input[name=ratings]:checked";
