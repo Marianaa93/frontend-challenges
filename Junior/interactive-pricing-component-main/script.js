c7fce7f94c83ae7e59bfa4d2d67aa777f5bedca5
Mariana Maia <marianalima.amb@gmail.com>
2022-12-27 14:44:22 -0300

    discount apllied

--- Junior/interactive-pricing-component-main/script.js
+++ Junior/interactive-pricing-component-main/script.js
@@ -1,24 +1,38 @@
 const price = document.getElementById("price");
 const views = document.getElementById("views");
 const slider = document.querySelector("#slider");
+const discountApplied = document.getElementById("discount");
 
-slider.oninput = function () {
-  if (slider.value <= 8) {
-    price.innerHTML = "8.00";
+function appliedDiscount() {
+  let sliderValue = slider.value;
+
+  if (!discountApplied.checked) {
+    price.textContent = sliderValue + ".00";
+  } else {
+    price.textContent = sliderValue * 0.25 + ".00";
+  }
+}
+
+function labelFormat() {
+  if (slider.value == 8) {
     views.innerHTML = "10K";
-  } else if ((slider.value > 8) & (slider.value <= 12)) {
-    price.innerHTML = "12.00";
+  } else if (slider.value == 12) {
     views.innerHTML = "50K";
-  } else if ((slider.value > 12) & (slider.value <= 16)) {
-    price.innerHTML = "16.00";
+  } else if (slider.value == 16) {
     views.innerHTML = "100K";
-  } else if ((slider.value > 16) & (slider.value <= 24)) {
-    price.innerHTML = "24.00";
+  } else if (slider.value == 24) {
     views.innerHTML = "500K";
-  } else if ((slider.value > 24) & (slider.value <= 36)) {
-    price.innerHTML = "36.00";
+  } else if (slider.value == 36) {
     views.innerHTML = "1M";
   }
+}
+
+slider.oninput = function () {
+  labelFormat();
+  appliedDiscount();
 };
 
-slider.addEventListener("mousemove", () => {});
+slider.addEventListener("mousemove", () => {
+  labelFormat();
+  discountApplied();
+});
