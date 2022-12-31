005bf906028112d4132ac703e7ebdd5eba7dc030
Mariana Maia <marianalima.amb@gmail.com>
2022-12-31 02:10:09 -0300

    implemented calendar

--- intermediate/launch-countdown-timer-main/script.js
+++ intermediate/launch-countdown-timer-main/script.js
@@ -3,23 +3,40 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
+const calendar = document.getElementById("calendar");
 
-function timeUptade(time) {
-  const timeinhours = time * 24;
-  const countToDate = new Date().setHours(new Date().getHours() + timeinhours);
+// function formatDate(timeReceived) {
+//   let d = new Date(timeReceived),
+//     month = "" + (d.getMonth() + 1),
+//     day = "" + d.getDate(),
+//     year = d.getFullYear();
 
-  setInterval(() => {
-    const currentDate = new Date();
-    const timeBetweenDates = Math.floor((countToDate - currentDate) / 1000);
-    const d = Math.floor(timeBetweenDates / (24 * 3600));
-    const hrs = Math.floor((timeBetweenDates / 3600) % 24);
-    const min = Math.floor((timeBetweenDates / 60) % 60);
-    const sec = Math.floor(timeBetweenDates % 60);
+//   if (month.length < 2) month = "0" + month;
+//   if (day.length < 2) day = "0" + day;
 
-    day.innerHTML = d;
-    hours.innerText = hrs;
-    minutes.innerText = min;
-    seconds.innerText = sec;
-  }, 1000);
+//   return [year, month, day].join("-");
+// }
+
+function timeUpdate() {
+  calendar.addEventListener("change", () => {
+    const informedTime = calendar.value;
+
+    const countToDate = new Date(informedTime);
+    console.log(countToDate);
+    // const test = new Date().setHours(new Date().getHours() + 240);
+    setInterval(() => {
+      const currentDate = new Date();
+      const timeBetweenDates = Math.floor((countToDate - currentDate) / 1000);
+      const d = Math.floor(timeBetweenDates / (24 * 3600));
+      const hrs = Math.floor((timeBetweenDates / 3600) % 24);
+      const min = Math.floor((timeBetweenDates / 60) % 60);
+      const sec = Math.floor(timeBetweenDates % 60);
+
+      day.innerHTML = String(d).padStart(2, "0");
+      hours.innerText = String(hrs).padStart(2, "0");
+      minutes.innerText = String(min).padStart(2, "0");
+      seconds.innerText = String(sec).padStart(2, "0");
+    }, 1000);
+  });
 }
-timeUptade(14);
+timeUpdate();
